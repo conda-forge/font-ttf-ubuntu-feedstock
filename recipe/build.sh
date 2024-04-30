d212b8c1d9d8ab48b6052db7cc2af1200c1408c3
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-04-30 14:55:00 +0300

    Add some debug statements
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+set -xe
+
 mkdir -p ${PREFIX}/fonts || true
 # flatten by 1: automatic flattening does not always happen due to __MACOSX/* files
 mv ubuntu-font-family-${PKG_VERSION}/* . || true
 install -v -m644 *.ttf ${PREFIX}/fonts/
 
+pwd
+ls -la ${PREFIX}
+ls -la ${RECIPE_ROOT}
+
 # add read file permissions to LICENCE.txt
-for folder in "./" "${PREFIX}" "${RECIPE_SRC}"; do
+for folder in "./" "${PREFIX}" "${RECIPE_ROOT}" "${SRC_DIR}"; do
     find ${folder} -name "LICENCE.txt" -exec chmod a+r {} \;
 done
