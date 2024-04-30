5ccf5ad9868f45b0b6ba31426bf774cc4fdab772
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-04-30 15:21:53 +0300

    Change the file permissions only for ${RECIPE_ROOT}/LICENCE.txt
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,15 +7,5 @@ mkdir -p ${PREFIX}/fonts || true
 mv ubuntu-font-family-${PKG_VERSION}/* . || true
 install -v -m644 *.ttf ${PREFIX}/fonts/
 
-pwd
-ls -la ${PREFIX}
-ls -la ${RECIPE_ROOT}
-
 # add read file permissions to LICENCE.txt
-for folder in "./" "${PREFIX}" "${RECIPE_ROOT}" "${SRC_DIR}"; do
-    for file in $(find ${folder} -name "LICENCE.txt"); do
-        ls -la ${file}
-        chmod +r ${file}
-        ls -la ${file}
-    done
-done
+chmod +r ${SRC_DIR}/LICENCE.txt
