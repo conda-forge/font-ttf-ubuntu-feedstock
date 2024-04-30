37b9e061c6502c2bef3f8b3d28724dd933a27678
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-04-30 14:37:48 +0300

    Fixes #2 - Add read permissions to LICENCE.txt
    conda-build later copies it at
    https://github.com/conda/conda-build/blob/e235104aca7c6bc0039a10f5ba2f2889c214c24d/conda_build/build.py#L821
    as is.
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,3 +4,8 @@ mkdir -p ${PREFIX}/fonts || true
 # flatten by 1: automatic flattening does not always happen due to __MACOSX/* files
 mv ubuntu-font-family-${PKG_VERSION}/* . || true
 install -v -m644 *.ttf ${PREFIX}/fonts/
+
+# add read file permissions to LICENCE.txt
+for folder in "./" "${PREFIX}" "${RECIPE_SRC}"; do
+    find ${folder} -name "LICENCE.txt" -exec chmod a+r {} \;
+done
